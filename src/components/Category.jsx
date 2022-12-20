4d2064296043108b4f768c89ba96916beb213961
Anastasiia Obushenko <n.obushenko@gmail.com>
2022-12-20 18:29:50 -0500

    Routing set up

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -8,19 +8,19 @@ import React from 'react';
 function Category() {
     return (
         <List>
-            <NavLink >
+            <NavLink to={'/cuisine/Italian'}>
                 <FaPizzaSlice />
                 <h4>Italian</h4>
             </NavLink>
-            <NavLink >
+            <NavLink to={'/cuisine/American'}>
                 <FaHamburger />
                 <h4>American</h4>
             </NavLink>
-            <NavLink >
+            <NavLink to={'/cuisine/Thai'}>
                 <GiNoodles />
                 <h4>Thai</h4>
             </NavLink>
-            <NavLink >
+            <NavLink to={'/cuisine/Japanese'}>
                 <GiChopsticks />
                 <h4>Japanese</h4>
             </NavLink>
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,12 +1,18 @@
 import Home from "./Home";
-import React from 'react'
+import Cuisine from "./Cuisine";
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
 
 function Pages() {
   return (
-      <div>
-          <Home />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine" element={<Cuisine />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default Pages
+export default Pages;
