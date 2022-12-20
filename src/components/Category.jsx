e796ebadf69aa297e4b523fafe6ff078d035d285
Anastasiia Obushenko <n.obushenko@gmail.com>
2022-12-20 17:55:26 -0500

    Category component items set as lins + style

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,30 +1,37 @@
 import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
 import { GiNoodles, GiChopsticks } from 'react-icons/gi';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
-import React from 'react'
+import React from 'react';
 
 function Category() {
-  return (
-      <div>
-          <div>
-              <FaPizzaSlice />
-              <h4>Italian</h4>
-          </div>
-          <div>
-              <FaHamburger />
-              <h4>American</h4>
-          </div>
-          <div>
-              <GiNoodles />
-              <h4>Thai</h4>
-          </div>
-          <div>
-              <GiChopsticks />
-              <h4>Japanese</h4>
-          </div>
-    </div>
-  )
+    return (
+        <List>
+            <NavLink >
+                <FaPizzaSlice />
+                <h4>Italian</h4>
+            </NavLink>
+            <NavLink >
+                <FaHamburger />
+                <h4>American</h4>
+            </NavLink>
+            <NavLink >
+                <GiNoodles />
+                <h4>Thai</h4>
+            </NavLink>
+            <NavLink >
+                <GiChopsticks />
+                <h4>Japanese</h4>
+            </NavLink>
+        </List>
+    );
 }
 
-export default Category
+const List = styled.div`
+display: flex;
+justify-content: center;
+margin: 2rem 0rem;
+`;
+
+export default Category;
