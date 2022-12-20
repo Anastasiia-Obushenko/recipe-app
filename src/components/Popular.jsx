789f7b22f26d53f898480ebc2f872927c0435df7
Anastasiia Obushenko <n.obushenko@gmail.com>
2022-12-20 16:05:30 -0500

    local storage config

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -12,14 +12,23 @@ function Popular() {
     }, []);
 
     const getPopular = async () => {
-        const api = await fetch(
-            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-        );
 
-        const data = await api.json();
-        setPopular(data.recipes);
+        const check = localStorage.getItem("popular");
+
+        if (check) {
+            setPopular(JSON.parse(check));
+        } else {
+            const api = await fetch(
+                `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+            );
+            const data = await api.json();
+            localStorage.setItem("popular", JSON.stringify(data.recipes))
+            setPopular(data.recipes);
+            console.log(check);
+        }
 
     };
+
     return (
         <div >
             <Wrapper>
@@ -35,7 +44,7 @@ function Popular() {
                 >
                     {popular.map((recipe) => {
                         return (
-                            <SplideSlide>
+                            <SplideSlide key={recipe.id}>
                                 <Card>
                                     <p>{recipe.title}</p>
                                     <img src={recipe.image} alt={recipe.title} />
