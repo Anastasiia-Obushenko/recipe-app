35abdd632f43969aba48688d5e58073f169338e3
Anastasiia Obushenko <n.obushenko@gmail.com>
2022-12-22 19:08:16 -0500

    motion set up

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -22,11 +22,16 @@ function Cuisine() {
     }, [params.type]);
 
     return (
-        <Grid>
+        <Grid
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{duration:0.5}}
+        >
             {cuisine.map((item) => {
                 return (
                     <Card key={item.id}>
-                        <Link to={ "/recipe/"+item.id}>
+                        <Link to={"/recipe/" + item.id}>
                             <img src={item.image} alt={item.title} />
                             <h4>{item.title}</h4>
                         </Link>
@@ -37,7 +42,7 @@ function Cuisine() {
     );
 }
 
-const Grid = styled.div`
+const Grid = styled(motion.div)`
 display: grid;
 grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
 grid-gap: 3rem;
